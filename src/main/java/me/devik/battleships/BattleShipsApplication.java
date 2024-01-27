a81fc44fcd12b55eec5d680980ef11e517b47b92
JMaslo <hry.v@post.cz>
2024-01-27 17:03:39 +0100

    New project

---
+++ src/main/java/me/devik/battleships/BattleShipsApplication.java
@@ -0,0 +1,18 @@
+package me.devik.battleships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BattleShipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleShipsApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/me/devik/battleships/BattleShipsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devik.battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleShipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
