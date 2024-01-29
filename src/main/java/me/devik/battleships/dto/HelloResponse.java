1cd399ce22d5ac8da9b81ab7965557bbaec3b7bb
Ondra <tethal@email.cz>
2024-01-29 16:42:28 +0100

    STOMP demo

---
+++ src/main/java/me/devik/battleships/dto/HelloResponse.java
@@ -0,0 +1,10 @@
+package me.devik.battleships.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloResponse {
+    private String message;
+}
